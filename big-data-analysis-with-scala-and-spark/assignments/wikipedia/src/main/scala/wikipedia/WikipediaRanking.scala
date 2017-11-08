e30dd1fe4aa04008e757b414a1f146082d0dd90e
liangyuanruo <liangyuanruo@gmail.com>
2017-11-08 17:12:33 +0800

    completed Wikipedia

--- big-data-analysis-with-scala-and-spark/assignments/wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
+++ big-data-analysis-with-scala-and-spark/assignments/wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
@@ -32,7 +32,10 @@ object WikipediaRanking {
    *  Hint1: consider using method `aggregate` on RDD[T].
    *  Hint2: consider using method `mentionsLanguage` on `WikipediaArticle`
    */
-  def occurrencesOfLang(lang: String, rdd: RDD[WikipediaArticle]): Int = ???
+  def occurrencesOfLang(lang: String, rdd: RDD[WikipediaArticle]): Int = {
+    rdd.filter(x => x.mentionsLanguage(lang))
+      .count().toInt
+  }
 
   /* (1) Use `occurrencesOfLang` to compute the ranking of the languages
    *     (`val langs`) by determining the number of Wikipedia articles that
@@ -42,12 +45,23 @@ object WikipediaRanking {
    *   Note: this operation is long-running. It can potentially run for
    *   several seconds.
    */
-  def rankLangs(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = ???
+  def rankLangs(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = {
+    if (langs.isEmpty) List()
+    else langs.map(lang => (lang, occurrencesOfLang(lang, rdd)))
+      .sortWith(_._2 > _._2) //descending sort on second element
+  }
 
   /* Compute an inverted index of the set of articles, mapping each language
    * to the Wikipedia pages in which it occurs.
    */
-  def makeIndex(langs: List[String], rdd: RDD[WikipediaArticle]): RDD[(String, Iterable[WikipediaArticle])] = ???
+  def makeIndex(langs: List[String], rdd: RDD[WikipediaArticle]): RDD[(String, Iterable[WikipediaArticle])] = {
+    rdd.flatMap(article =>
+      for {
+        lang <- langs
+        if article.mentionsLanguage(lang)
+      } yield (lang, article))
+      .groupByKey()
+  }
 
   /* (2) Compute the language ranking again, but now using the inverted index. Can you notice
    *     a performance improvement?
@@ -55,7 +69,10 @@ object WikipediaRanking {
    *   Note: this operation is long-running. It can potentially run for
    *   several seconds.
    */
-  def rankLangsUsingIndex(index: RDD[(String, Iterable[WikipediaArticle])]): List[(String, Int)] = ???
+  def rankLangsUsingIndex(index: RDD[(String, Iterable[WikipediaArticle])]): List[(String, Int)] = {
+    index.mapValues(articles => articles.size)
+      .collect().toList.sortWith(_._2 > _._2)
+  }
 
   /* (3) Use `reduceByKey` so that the computation of the index and the ranking are combined.
    *     Can you notice an improvement in performance compared to measuring *both* the computation of the index
@@ -64,7 +81,16 @@ object WikipediaRanking {
    *   Note: this operation is long-running. It can potentially run for
    *   several seconds.
    */
-  def rankLangsReduceByKey(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = ???
+  def rankLangsReduceByKey(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = {
+    rdd.flatMap(article =>
+      for {
+        lang <- langs
+        if article.mentionsLanguage(lang)
+      } yield (lang, 1))
+      .reduceByKey((lang, cnt) => cnt + cnt)
+      .collect().toList
+      .sortWith(_._2 > _._2)
+  }
 
   def main(args: Array[String]) {
 
--- big-data-analysis-with-scala-and-spark/assignments/wikipedia/target/scala-2.11/classes/wikipedia/test.sc
+++ big-data-analysis-with-scala-and-spark/assignments/wikipedia/target/scala-2.11/classes/wikipedia/test.sc
@@ -1,9 +1,38 @@
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.SparkContext._
-
-val x = 5;
+import org.apache.spark.rdd.RDD
+import wikipedia.WikipediaArticle
+import wikipedia.WikipediaData
 
 val conf: SparkConf = new SparkConf().setMaster("local").setAppName("wikipedia")
 val sc: SparkContext = new SparkContext(conf)
 
+val langs = List(
+  "JavaScript", "Java", "PHP", "Python", "C#", "C++", "Ruby", "CSS",
+  "Objective-C", "Perl", "Scala", "Haskell", "MATLAB", "Clojure", "Groovy")
+
+def parse(line: String): WikipediaArticle = {
+  val subs = "</title><text>"
+  val i = line.indexOf(subs)
+  val title = line.substring(14, i)
+  val text  = line.substring(i + subs.length, line.length-16)
+  WikipediaArticle(title, text)
+}
+
+// Hint: use a combination of `sc.textFile`, `WikipediaData.filePath` and `WikipediaData.parse`
+val wikiRdd: RDD[WikipediaArticle] =
+  sc.textFile("/Users/yuanruoliang/Developer/coursera/big-data-analysis-with-scala-and-spark/assignments/wikipedia/src/main/resources/wikipedia/wikipedia.dat")
+    .map(parse)
+
+/** Returns the number of articles on which the language `lang` occurs.
+  *  Hint1: consider using method `aggregate` on RDD[T].
+  *  Hint2: consider using method `mentionsLanguage` on `WikipediaArticle`
+  */
+def occurrencesOfLang(lang: String, rdd: RDD[WikipediaArticle]): Int = {
+  1
+}
+
+wikiRdd.take(5).foreach(println)
+
 sc.stop()
+
